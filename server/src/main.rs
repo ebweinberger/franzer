03795f52e372151293f6914ebae370bfe7b6bf03
Eli Weinberger <eliweinberger@hey.com>
2024-12-07 23:08:45 -0500

    new create endpoint with some validation

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,9 @@
-use axum::{http::StatusCode, routing::post, Json, Router};
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::post,
+    Error, Json, Router,
+};
 
 use serde::{Deserialize, Serialize};
 
@@ -8,7 +13,9 @@ async fn main() {
     const PORT: &str = "7878";
     let endpoint: String = HOST.to_owned() + ":" + PORT;
 
-    let app = Router::new().route("/entries", post(entries));
+    let app = Router::new()
+        .route("/entries", post(entries))
+        .route("/create_entry", post(create_entry));
     let listener = tokio::net::TcpListener::bind(endpoint).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 
@@ -24,6 +31,26 @@ async fn entries(Json(payload): Json<EntriesRequest>) -> (StatusCode, Json<Entri
     return (StatusCode::OK, Json(response));
 }
 
+async fn create_entry(Json(payload): Json<CreateEntryRequest>) -> Response {
+    if payload.content_type == ContentType::File {
+        return (
+            StatusCode::NOT_IMPLEMENTED,
+            "Content type 'file' is not implemented yet",
+        )
+            .into_response();
+    }
+
+    // println!("{}", payload);
+
+    let response = CreateEntryResponse {
+        entry: Entry {
+            id: 1,
+            content_type: payload.content_type,
+        },
+    };
+    return (StatusCode::CREATED, Json(response)).into_response();
+}
+
 #[derive(Deserialize)]
 struct EntriesRequest {
     namespace: String,
@@ -32,4 +59,29 @@ struct EntriesRequest {
 #[derive(Serialize)]
 struct EntriesResponse {
     entries: u8,
+}
+
+#[derive(Deserialize)]
+struct CreateEntryRequest {
+    namespace: String,
+    content_type: ContentType,
+    content: String,
+}
+
+#[derive(Serialize)]
+struct CreateEntryResponse {
+    entry: Entry,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Entry {
+    id: u64,
+    content_type: ContentType,
+}
+
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+#[serde(rename_all = "lowercase")]
+enum ContentType {
+    Text,
+    File,
 }
